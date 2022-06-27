636b58eec347e62504cd66b6260c984b13bedd45
CANDELA Nicolas <darkxell.mc@gmail.com>
2022-06-27 18:07:29 +0200

    FIxed log overrides

--- LoggingServer/src/main/java/fr/darkxell/LoggingServer/LogFileWriter.java
+++ LoggingServer/src/main/java/fr/darkxell/LoggingServer/LogFileWriter.java
@@ -1,16 +1,61 @@
 package fr.darkxell.LoggingServer;
 
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.PrintWriter;
+import java.sql.Date;
+import java.util.ArrayList;
 
-public class LogFileWriter {
+public class LogFileWriter implements Runnable {
+
+	public static ArrayList<String> logStack = new ArrayList<>(50);
+
+	private static LogFileWriter instance = null;
 
 	public static void log(String line) {
-		try (PrintWriter out = new PrintWriter("gametunut.log")) { //FIXME: make the file not override everything
-		    out.println(line);
+		logStack.add(line);
+		if (instance == null) {
+			instance = new LogFileWriter();
+			Thread t = new Thread(instance);
+			t.start();
+		}
+	}
+
+	private void logToFile(String line) {
+		try (PrintWriter out = new PrintWriter(new FileOutputStream(generateFileName(), true))) {
+			out.println(line);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
+	private String generateFileName() {
+		try {
+			Date date = new Date(System.currentTimeMillis());
+			return date.toString() + "-gametunut.log";
+		} catch (Exception e) {
+			return "gametunut-default.log";
+		}
+	}
+
+	@Override
+	public void run() {
+		for (;;) {
+			try {
+				while (logStack.size() >= 1) {
+					String tolog = logStack.get(0);
+					logStack.remove(0);
+					logToFile(tolog);
+				}
+			} catch (Exception e) {
+				System.err.println("Couldn't log " + logStack.size() + " lines, trying again in 1 second...");
+				e.printStackTrace();
+			}
+			try {
+				Thread.sleep(1000);
+			} catch (Exception e) {
+			}
+		}
+	}
+
 }
